8e15c9b6ace5197de5f6617d19f7650ee50b3d5b
pedro-cristino <pedro.cristino.pro@gmail.com>
2019-08-03 22:14:11 +0200

    Read file where python script is writing

--- index.php
+++ index.php
@@ -6,17 +6,35 @@
     <title>Basketball arcade by Pedro Cristino</title>
   </head>
   <body style="background:black;color:red;font-size: 5em;">
-    <table style="width:100%">
+  
+  <script>
+	let pid = 0;
+	function loadDoc() {
+	  var xhttp = new XMLHttpRequest();
+	  xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+		  const rep = this.responseText;
+		  document.getElementById("currentScore").innerHTML = rep.split(",")[0];
+		  pid = rep.split(",")[1];
+		}
+	  };
+	  xhttp.open("GET", "readfile.php", true);
+	  xhttp.send();
+	}
+  </script>
+    <table style="width:100%;border:1px solid white">
 		<tr>
 			<th>Best Score</th>
 			<th>Your Score</th>
 			<th>Time Left</th>
 		</tr>
 		<tr>
-			<th>250</th>
-			<th>224</th>
-			<th>50s</th>
+			<th id="bestScore">250</th>
+			<th id="currentScore">224</th>
+			<th id="remainingTime">50s</th>
 		</tr>
 	</table>
   </body>
+  
+  
 </html>
---
+++ readfile.php
@@ -0,0 +1,6 @@
+<?php 
+$file = file_get_contents('./test.data', FILE_USE_INCLUDE_PATH);
+
+echo $file;
+
+?>
