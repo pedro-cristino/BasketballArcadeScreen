97093e23ed6cc4e69be61819c2ce28161fdd2ed7
pedro-cristino <pedro.cristino.pro@gmail.com>
2019-08-03 21:00:03 +0200

    Basic template

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php ?>
-
-SALUT
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Basketball arcade by Pedro Cristino</title>
+  </head>
+  <body style="background:black;color:red">
+    <table>
+		<tr>
+			<th>Best Score</th>
+			<th>Your Score</th>
+			<th>Time Left</th>
+		</tr>
+		<tr>
+			<td>250</td>
+			<td>224</td>
+			<td>50s</td>
+		</tr>
+	</table>
+  </body>
+</html>
