81c9fb05cda41559f4282a47c2b98ba2fb21237a
pedro-cristino <pedro.cristino.pro@gmail.com>
2019-08-03 21:03:56 +0200

    Obligé de commit pour tester côté serveur :P

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
     <meta charset="utf-8">
     <title>Basketball arcade by Pedro Cristino</title>
   </head>
-  <body style="background:black;color:red">
-    <table>
+  <body style="background:black;color:red;font-size: 5em;">
+    <table style="width:100%">
 		<tr>
 			<th>Best Score</th>
 			<th>Your Score</th>
 			<th>Time Left</th>
 		</tr>
 		<tr>
-			<td>250</td>
-			<td>224</td>
-			<td>50s</td>
+			<th>250</th>
+			<th>224</th>
+			<th>50s</th>
 		</tr>
 	</table>
   </body>
